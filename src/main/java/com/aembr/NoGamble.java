09e8ba4f110f2f9a03d7678dff9d17627cc525e4
aembr <aembr@duck.com>
2023-04-09 14:21:03 +0100

    Rename to NoGamble

---
+++ src/main/java/com/aembr/NoGamble.java
@@ -0,0 +1,15 @@
+package com.aembr;
+
+import net.minecraftforge.fml.common.Mod;
+
+
+@Mod(
+		modid = NoGamble.MODID,
+		name = NoGamble.NAME,
+		version = NoGamble.VERSION
+)
+public class NoGamble {
+	public static final String MODID = "nogamble";
+	public static final String NAME = "NoGamble";
+	public static final String VERSION = "0.2";
+}
