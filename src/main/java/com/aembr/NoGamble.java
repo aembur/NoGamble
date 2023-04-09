e82f6de83bf7d37a2aef2a59ad8b58ce283e7f11
aembr <aembr@duck.com>
2023-04-09 15:04:43 +0100

    Create default config

--- src/main/java/com/aembr/NoGamble.java
+++ src/main/java/com/aembr/NoGamble.java
@@ -1,7 +1,14 @@
 package com.aembr;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.regex.Pattern;
 
 @Mod(
 		modid = NoGamble.MODID,
@@ -12,4 +19,20 @@ public class NoGamble {
 	public static final String MODID = "nogamble";
 	public static final String NAME = "NoGamble";
 	public static final String VERSION = "0.2";
+
+	private static final String CONFIG_FILE_NAME = "nogamble.json";
+
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		File configFile = new File(event.getModConfigurationDirectory(), CONFIG_FILE_NAME);
+		if (!configFile.exists()) {
+			try {
+				InputStream defaultConfigStream = getClass().getResourceAsStream("/default-config.json");
+				Files.copy(defaultConfigStream, configFile.toPath());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
 }
